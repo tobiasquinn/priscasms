cff07ec6adbe9f5c1416e0b35abfdb49663910d1
Tobias Quinn <tobias@tobiasquinn.com>
2011-01-16 18:16:47 +0000

    Corrected setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import os, shutil
+
+# Build UI code
+os.system('make')
+shutil.copyfile('priscasms.py', 'priscasms/priscasms')
+
+setup(name='PriscaSMS',
+        version="0.01",
+        description="QT GUI to send SMS using gammu",
+        author="Tobias Quinn",
+        author_email="tobias@tobiasquinn.com",
+        url="http://tobiasquinn.com/code/priscasms.html",
+        packages=['priscasms', 'priscasms.ui'],
+        scripts=['priscasms/priscasms']
+        )
+
+# Cleanup
+os.system('make clean')
+os.remove('priscasms/priscasms')
+
