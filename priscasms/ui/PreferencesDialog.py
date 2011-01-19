fb5d052aae1146cfb2754f8ae8bbcd0d17d55c46
Tobias Quinn <tobias@tobiasquinn.com>
2011-01-19 13:01:26 +0000

    Changed to work around bug reading bools cross platform properly

--- priscasms/ui/PreferencesDialog.py
+++ priscasms/ui/PreferencesDialog.py
@@ -5,6 +5,16 @@ from PyQt4.QtCore import QVariant, QSettings, SIGNAL
 from .PreferencesDialogUI import Ui_PreferencesDialog
 
 class PreferencesDialog(QtGui.QDialog):
+    def _boolPrefValueLoad(self, value):
+        # This deals with bug in mapping from string to bool in pyqt4 4.8.2 to be fixed in 4.8.3
+        if type(value) != bool:
+            if value == 'true':
+                return True
+            else:
+                return False
+        else:
+            return value
+
     def __init__(self, parent):
         QtGui.QDialog.__init__(self, parent)
         self.ui = Ui_PreferencesDialog()
@@ -13,11 +23,9 @@ class PreferencesDialog(QtGui.QDialog):
         self.connect(self, SIGNAL("finished(int)"), self._result)
         # load preferences
         self._settings = QSettings()
-        print(type(self._settings.value("sms/sshmode", False)))
         #self.ui.checkBoxSSH.setChecked(self._settings.value("sms/sshmode", False type=bool)))
-        # Note this is a hack for pyqt4 version 4.8.2 which sould be fixed in 4.8.3
-        # Is this cross platform???
-        self.ui.checkBoxSSH.setChecked(self._settings.value("sms/sshmode", False) == "true")
+        # NOTE: this is a hack for pyqt4 version 4.8.2 which sould be fixed in 4.8.3
+        self.ui.checkBoxSSH.setChecked(self._boolPrefValueLoad(self._settings.value("sms/sshmode", False)))
         self.ui.lineEditSSHAddress.setText(self._settings.value("sms/sshaddress"))
 
     def _result(self, result):
