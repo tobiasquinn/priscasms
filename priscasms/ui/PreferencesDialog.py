70635f5c556cf6f9a48687a92b61d5848e44f7ba
Tobias Quinn <tobias@tobiasquinn.com>
2011-01-28 14:36:16 +0000

    changed to new pyqt 4.8.3 qsettings api got bool

--- priscasms/ui/PreferencesDialog.py
+++ priscasms/ui/PreferencesDialog.py
@@ -5,16 +5,6 @@ from PyQt4.QtCore import QVariant, QSettings, SIGNAL
 from .PreferencesDialogUI import Ui_PreferencesDialog
 
 class PreferencesDialog(QtGui.QDialog):
-    def _boolPrefValueLoad(self, value):
-        # This deals with bug in mapping from string to bool in pyqt4 4.8.2 to be fixed in 4.8.3
-        if type(value) != bool:
-            if value == 'true':
-                return True
-            else:
-                return False
-        else:
-            return value
-
     def __init__(self, parent):
         QtGui.QDialog.__init__(self, parent)
         self.ui = Ui_PreferencesDialog()
@@ -23,9 +13,7 @@ class PreferencesDialog(QtGui.QDialog):
         self.connect(self, SIGNAL("finished(int)"), self._result)
         # load preferences
         self._settings = QSettings()
-        #self.ui.checkBoxSSH.setChecked(self._settings.value("sms/sshmode", False type=bool)))
-        # NOTE: this is a hack for pyqt4 version 4.8.2 which sould be fixed in 4.8.3
-        self.ui.checkBoxSSH.setChecked(self._boolPrefValueLoad(self._settings.value("sms/sshmode", False)))
+        self.ui.checkBoxSSH.setChecked(self._settings.value("sms/sshmode", False, bool))
         self.ui.lineEditSSHAddress.setText(self._settings.value("sms/sshaddress").toString())
 
     def _result(self, result):
